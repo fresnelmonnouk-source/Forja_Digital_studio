2362149e0ecdea58b7aed1a56badf9f467239cf0
Fresnel <fresnel@forja.fr>
2026-05-19 08:11:43 +0200

    fix: Simplify Sentry server config (remove custom integration)

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,9 +5,4 @@ Sentry.init({
   environment: process.env.NODE_ENV,
   tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0,
   debug: process.env.NODE_ENV !== "production",
-  integrations: [
-    Sentry.httpIntegration({
-      statusCodes: [[500, 599]],
-    }),
-  ],
 });
